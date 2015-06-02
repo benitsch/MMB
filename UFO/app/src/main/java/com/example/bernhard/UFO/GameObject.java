b8830f2d1f2b9f60d723f543f1ef8f4fe57e77c3
benitsch <BernhardNitsch@gmx.at>
2015-06-02 22:29:08 +0200

    erst hochladen

---
+++ UFO/app/src/main/java/com/example/bernhard/UFO/GameObject.java
@@ -0,0 +1,45 @@
+package com.example.bernhard.UFO;
+
+import android.graphics.Rect;
+
+/**
+ * Created by Bernhard on 24.05.2015.
+ */
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getWidth()
+    {
+        return width;
+    }
+    public Rect getRectangle()
+    {
+        return new Rect(x, y, x+width, y+height);
+    }
+
+}
